8d564d6579c4b30bc0a9c25033726a3c39b315b2
Julian Manke <julian.manke@googlemail.com>
2025-11-18 19:10:10 +0100

    healthkit int

---
+++ GymStreakWatch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GymStreakWatch Watch App
+//
+//  Created by Julian Manke on 18.11.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GymStreakWatch Watch App/GymStreakWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymStreakWatchApp.swift
+//  GymStreakWatch Watch App
+//
+//  Created by Julian Manke on 18.11.25.
+//
+
+import SwiftUI
+
+@main
+struct GymStreakWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
